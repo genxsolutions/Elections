946050568d83ca19131fd5d727c2a8c6da45cd6a
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-04-23 16:23:15 +0100

    refactored app to inject pass data service through the layers

---
+++ appWithComposeUI/src/main/java/com/genxsol/elections/ElectionsApplication.kt
@@ -0,0 +1,8 @@
+package com.genxsol.elections
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ElectionsApplication : Application()
+//TODO initialise work manager and other MI tracking stuff if need to be
---
+++ appWithComposeUI/src/main/java/com/genxsol/elections/ui/ElectionsActivity.kt
@@ -0,0 +1,25 @@
+package com.genxsol.elections.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import com.genxsol.elections.ui.base.MyNavHost
+import com.genxsol.elections.ui.theme.ElectionAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ElectionsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ElectionAppTheme {
+                Surface {
+                    MyNavHost()
+                }
+            }
+        }
+    }
+
+}
