b443587167717994ac52b95223d95d6b17d93be8
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-04-24 09:39:07 +0100

    some refactoring around viewmodel ui state

--- appWithComposeUI/src/main/java/com/genxsol/elections/ui/viewmodels/ElectionResultsViewModel.kt
+++ appWithComposeUI/src/main/java/com/genxsol/elections/ui/viewmodels/ElectionResultsViewModel.kt
@@ -4,23 +4,17 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.utilities.NetworkHelper
-import com.genxsol.elections.api.Results
 import com.genxsol.elections.common.NoInternetException
 import com.genxsol.elections.common.dispatcher.DispatcherProvider
 import com.genxsol.elections.common.logger.Logger
-import com.genxsol.elections.common.util.sortResultsByVotes
-import com.genxsol.elections.data.database.entity.CandidateEntity
 import com.genxsol.elections.data.repository.ElectionsRepository
 import com.genxsol.elections.ui.base.ResultScreenUiState
-import com.genxsol.elections.ui.base.ResultUiState
 import com.genxsol.elections.ui.base.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -47,46 +41,25 @@ class ElectionResultsViewModel @Inject constructor(
     private fun fetchPollResultData() {
         viewModelScope.launch(dispatcherProvider.io) {
             if (!networkHelper.isNetworkConnected()) {
-                handleNoInternet()
+                handleFetchError( NoInternetException() )
                 return@launch
             }
 
             try {
+                // concurrent loading to give results
                 val candidates = async { electionsRepository.getAllCandidates() }
                 val pollResults = async { electionsRepository.getPollResults() }
 
-                makeResultScreenUiState(pollResults.await(), candidates.await())
+                ElectionResultsViewModelState(
+                    pollResults.await(),
+                    candidates.await()
+                ).toResultUiState()
                     .catch { throwable -> handleFetchError(throwable) }
                     .collect { handleFetchSuccess(it) }
             } catch (e: Exception) {
                 handleFetchError(e)
             }
         }
-    }
-
-    private fun makeResultScreenUiState(
-        pollResults: Flow<Results>,
-        candidates: Flow<List<CandidateEntity>>
-    ): Flow<ResultScreenUiState> =
-
-        pollResults.combine(candidates) { polls, candidatesList ->
-            val resultUiStateList = polls.results.sortResultsByVotes().map { result ->
-                val candidateName =
-                    candidatesList.firstOrNull { it.id == result.candidateId }?.name ?: ""
-                val leading =
-                    polls.results.maxByOrNull { it.votes }?.candidateId == result.candidateId
-                ResultUiState(
-                    party = result.party,
-                    candidate = candidateName,
-                    votes = result.votes.toString(),
-                    leading = leading
-                )
-            }
-            ResultScreenUiState(resultUiStateList, polls.isComplete)
-        }
-
-    private fun handleNoInternet() {
-        _uiState.value = UIState.Failure(throwable = NoInternetException())
     }
 
     private fun handleFetchError(throwable: Throwable) {
--- appWithComposeUI/src/main/java/com/genxsol/elections/ui/viewmodels/ElectionResultsViewModelState.kt
+++ appWithComposeUI/src/main/java/com/genxsol/elections/ui/viewmodels/ElectionResultsViewModelState.kt
@@ -1,25 +1,32 @@
 package com.genxsol.elections.ui.viewmodels
 
-import com.genxsol.elections.api.Candidate
 import com.genxsol.elections.ui.base.ResultUiState
-import com.genxsol.elections.api.Result
+import com.genxsol.elections.api.Results
+import com.genxsol.elections.common.util.sortResultsByVotes
+import com.genxsol.elections.data.database.entity.CandidateEntity
 import com.genxsol.elections.ui.base.ResultScreenUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 
-data class ElectionResultsViewModelState(
-    val results: List<Result> = emptyList(),
-    val candidates: List<Candidate> = emptyList(),
-    val countingComplete: Boolean = false
+ class ElectionResultsViewModelState(
+    private val pollResults: Flow<Results>,
+    private val candidates: Flow<List<CandidateEntity>>
 ) {
+    fun toResultUiState(): Flow<ResultScreenUiState> =
 
-    fun toUiState() = ResultScreenUiState(
-        results = results.map { result ->
-            ResultUiState(
-                party = result.party,
-                candidate = result.candidateId.toString(),
-                votes = result.votes.toString(),
-                leading = false
-            )
-        },
-        complete = countingComplete
-    )
+        pollResults.combine(candidates) { polls, candidatesList ->
+            val resultUiStateList = polls.results.sortResultsByVotes().map { result ->
+                val candidateName =
+                    candidatesList.firstOrNull { it.id == result.candidateId }?.name ?: ""
+                val leading =
+                    polls.results.maxByOrNull { it.votes }?.candidateId == result.candidateId
+                ResultUiState(
+                    party = result.party,
+                    candidate = candidateName,
+                    votes = result.votes.toString(),
+                    leading = leading
+                )
+            }
+            ResultScreenUiState(resultUiStateList, polls.isComplete)
+        }
 }
